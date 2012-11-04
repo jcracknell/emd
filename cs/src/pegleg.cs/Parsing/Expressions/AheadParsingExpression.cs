f4b5077467669324393d5c3875e02a03af5d1718
James Cracknell <james.h.cracknell@gmail.com>
2012-11-04 16:37:21 -0700

    Add missing source file

---
+++ cs/src/pegleg.cs/Parsing/Expressions/AheadParsingExpression.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pegleg.cs.Parsing.Expressions {
+	public abstract class AheadParsingExpression<TBody, TProduct> : BaseParsingExpression<TProduct> {
+		protected readonly IParsingExpression<TBody> _body;
+
+		public AheadParsingExpression(IParsingExpression<TBody> body)
+			: base(ParsingExpressionKind.Lookahead)
+		{
+			CodeContract.ArgumentIsNotNull(() => body, body);
+
+			_body = body;
+		}
+
+		public IParsingExpression<TBody> Body { get { return _body; } }
+
+		public override T HandleWith<T>(IParsingExpressionHandler<T> handler) {
+			return handler.Handle(this);
+		}
+	}
+
+	public class NonCapturingAheadParsingExpression<TBody> : AheadParsingExpression<TBody, TBody> {
+		public NonCapturingAheadParsingExpression(IParsingExpression<TBody> body)
+			: base(body)
+		{ }
+
+		protected override IMatchingResult<TBody> MatchesCore(IMatchingContext context) {
+			return _body.Matches(context.Clone());
+		}
+	}
+
+	public class CapturingAheadParsingExpression<TBody, TProduct> : AheadParsingExpression<TBody, TProduct> {
+		private readonly Func<IMatch<TBody>, TProduct> _matchAction;
+
+		public CapturingAheadParsingExpression(IParsingExpression<TBody> body, Func<IMatch<TBody>, TProduct> matchAction)
+			: base(body)
+		{
+			CodeContract.ArgumentIsNotNull(() => matchAction, matchAction);
+			
+			_matchAction = matchAction;
+		}
+
+		protected override IMatchingResult<TProduct> MatchesCore(IMatchingContext context) {
+			var bodyMatchingContext = context.Clone();
+			var bodyMatchingResult = _body.Matches(bodyMatchingContext);
+
+			if(bodyMatchingResult.Succeeded) {
+				var product = _matchAction(context.StartMatch().CompleteMatch(this, bodyMatchingResult.Product));
+				return SuccessfulMatchingResult.Create(product);
+			}
+
+			return UnsuccessfulMatchingResult.Create(this);
+		}
+	}
+}
