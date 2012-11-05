eed58f086e2639b280afbc1aee58fcbcbf0aa46c
James Cracknell <james.h.cracknell@gmail.com>
2012-11-04 17:48:50 -0700

    Updated RuleTestingExtensionMethods
    Updated RuleTestingExtensionMethods to return generic
    IMatchingResult<TProduct> introduced in
    28ca870bca5eecfa34bece96b03a44a3571ae6ed

--- cs/src/markdom.cs.Tests/Grammar/RuleTestingExtensionMethods.cs
+++ cs/src/markdom.cs.Tests/Grammar/RuleTestingExtensionMethods.cs
@@ -9,19 +9,19 @@ using System.Threading.Tasks;
 
 namespace markdom.cs.Grammar {
 	public static class RuleTestingExtensionMethods {
-		public static IMatchingResult Match(this IParsingExpression expression, params string[] input) {
+		public static IMatchingResult<TProduct> Match<TProduct>(this IParsingExpression<TProduct> expression, params string[] input) {
 			var context = new MatchingContext(string.Join("", input));
 			var match = expression.Matches(context);
 			return match;
 		}
 
-		public static IMatchingResult AssertMatch(this IParsingExpression expression, params string[] input) {
+		public static IMatchingResult<TProduct> AssertMatch<TProduct>(this IParsingExpression<TProduct> expression, params string[] input) {
 			var match = expression.Match(input);
 			Assert.IsTrue(match.Succeeded, "Expression does not match.\nExpression:\n" + expression.ToString() + "\nInput:\n" + string.Join("", input));
 			return match;
 		}
 
-		public static IMatchingResult AssertNoMatch(this IParsingExpression expression, params string[] input) {
+		public static IMatchingResult<TProduct> AssertNoMatch<TProduct>(this IParsingExpression<TProduct> expression, params string[] input) {
 			var match = expression.Match(input);
 			Assert.IsTrue(match.Succeeded, "Expression matches.\nExpression:\n" + expression.ToString() + "\nInput:\n" + string.Join("", input));
 			return match;
