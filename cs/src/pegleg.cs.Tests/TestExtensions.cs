1b95d31066abdd099f49fae635915bbb098a9441
James Cracknell <james.h.cracknell@gmail.com>
2012-11-08 19:40:47 -0700

    Emptied pegleg.cs.Tests
    Removed all tests from pegleg.cs.Tests, as the existing tests are wildly out of date
    Reconfigured project for Xunit/FluentAssertions

---
+++ cs/src/pegleg.cs.Tests/TestExtensions.cs
@@ -0,0 +1,30 @@
+﻿using pegleg.cs;
+using pegleg.cs.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace pegleg.cs {
+	public static class RuleTestingExtensionMethods {
+		public static IMatchingResult<TProduct> ShouldMatch<TProduct>(this IParsingExpression<TProduct> expression, params string[] input) {
+			var context = new MatchingContext(string.Join("", input));
+			var match = expression.Matches(context);
+
+			match.Succeeded.Should().BeTrue();
+
+			return match;
+		}
+
+		public static IMatchingResult<TProduct> ShouldNotMatch<TProduct>(this IParsingExpression<TProduct> expression, params string[] input) {
+			var context = new MatchingContext(string.Join("", input));
+			var match = expression.Matches(context);
+
+			match.Succeeded.Should().BeFalse();
+
+			return match;
+		}
+	}
+}
