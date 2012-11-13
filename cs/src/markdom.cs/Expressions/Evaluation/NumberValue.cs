4fc6a1feba405aa32c8ee14da040a614b934d27f
James Cracknell <james.h.cracknell@gmail.com>
2012-11-12 22:35:30 -0700

    Add NumberValue

---
+++ cs/src/markdom.cs/Expressions/Evaluation/NumberValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace markdom.cs.Expressions.Evaluation {
+	public class NumberValue : IValue {
+		private readonly double _value;
+
+		public NumberValue(double value) {
+			_value = value;
+		}
+
+		public double Value { get { return _value; } }
+	}
+}
