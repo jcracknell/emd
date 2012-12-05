ed5edc623cb46bacd1f99c4fec4c780e94d8a81d
James Cracknell <james.h.cracknell@gmail.com>
2012-12-04 21:10:44 -0700

    Copy ExtensionMethods to pegleg.cs

---
+++ cs/src/pegleg.cs/ExtensionMethods.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pegleg.cs {
+	internal static class ExtensionMethods {
+		public static string JoinStrings(this IEnumerable<string> parts) {
+			return JoinStrings(parts, "");
+		}
+
+		public static string JoinStrings(this IEnumerable<string> parts, string separator) {
+			var sb = new StringBuilder();
+			var enumerator = parts.GetEnumerator();
+
+			if(enumerator.MoveNext())
+				sb.Append(enumerator.Current);
+
+			while(enumerator.MoveNext())
+				sb.Append(separator).Append(enumerator.Current);
+
+			return sb.ToString();
+		}
+
+		public static int ParseDefault(this string s, int otherwise) {
+			int v; return int.TryParse(s, out v) ? v : otherwise;
+		}
+
+		public static long ParseDefault(this string s, long otherwise) {
+			long v; return long.TryParse(s, out v) ? v : otherwise;
+		}
+
+		public static uint ParseDefault(this string s, uint otherwise) {
+			uint v; return uint.TryParse(s, out v) ? v : otherwise;
+		}
+
+		public static ulong ParseDefault(this string s, ulong otherwise) {
+			ulong v; return ulong.TryParse(s, out v) ? v : otherwise;
+		}
+
+		public static float ParseDefault(this string s, float otherwise) {
+			float v; return float.TryParse(s, out v) ? v : otherwise;
+		}
+
+		public static double ParseDefault(this string s, double otherwise) {
+			double v; return double.TryParse(s, out v) ? v : otherwise;
+		}
+
+		public static decimal ParseDefault(this string s, decimal otherwise) {
+			decimal v; return decimal.TryParse(s, out v) ? v : otherwise;
+		}
+
+		/// <summary>
+		/// Returns the value of the current <see cref="Nullable`1"/>, or the provided <paramref name="@default"/> value.
+		/// Concise way of writing <code>(nullable.HasValue ? nullable.Value : @default)</code>.
+		/// </summary>
+		public static T ValueOr<T>(this Nullable<T> nullable, T @default) where T : struct {
+			return nullable.HasValue ? nullable.Value : @default;
+		}
+
+		public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> enumerableEnumerable) {
+			return enumerableEnumerable.SelectMany(i => i);
+		}
+
+		public static IEnumerable<T> InEnumerable<T>(this T obj) {
+			yield return obj;
+		}
+
+		public static TMemo Reduce<T, TMemo>(this IEnumerable<T> enumerable, TMemo memo, Func<TMemo, T, TMemo> reduce) {
+			foreach(var item in enumerable)
+				memo = reduce(memo, item);
+			return memo;
+		}
+
+		public static IEnumerable<T> Each<T>(this IEnumerable<T> enumerable, Action<T> action) {
+			foreach(var item in enumerable)
+				action(item);
+			return enumerable;
+		}
+	}
+}
