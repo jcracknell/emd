881203f5d082ca436c7a0323655e723dffa828f3
James Cracknell <james.h.cracknell@gmail.com>
2012-12-13 21:33:59 -0700

    Add Emd gateway class

---
+++ cs/src/emd.cs/Emd.cs
@@ -0,0 +1,16 @@
+﻿using emd.cs.Grammar;
+using emd.cs.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace emd.cs {
+	public static class Emd {
+		public static DocumentNode ParseDocument(string input) {
+			if(null == input) throw ExceptionBecause.ArgumentNull(() => input);
+
+			return EmdGrammar.Document.Matches(new pegleg.cs.Parsing.MatchingContext(input)).Product;
+		}
+	}
+}
