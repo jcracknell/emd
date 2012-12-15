e41015d75f7c7638a1804131f570555152844423
James Cracknell <james.h.cracknell@gmail.com>
2012-12-15 00:34:06 -0700

    Improved structural test output formatting

--- cs/test/structural.sh
+++ cs/test/structural.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+PASS="$(echo -ne "\033[0;32m[PASS]\033[0m")"
+FAIL="$(echo -ne "\033[0;31m[FAIL]\033[0m")"
+
 for b in Debug Release; do
 	REMD="$(dirname "$0")/../src/remd/bin/$b/remd.exe";
 	if [ -f "$REMD" ]; then
@@ -20,29 +23,32 @@ find "$TESTS_FOLDER" -type f | grep -E '\.emd$' | while read test; do
 
 	expected="$(dirname "$test")/$(basename "$test" .emd).expected";
 	if [ ! -f "$expected" ]; then
-		echo "FAIL";
+		echo $FAIL;
 		echo -e "\tNot found: \"$expected\".";
 		continue;
 	fi
 
 	renderer="$(head -n 1 "$expected" | grep -E '^#!' | sed -r 's/^#!\s+//g')"
 	if [ ! "$renderer" ]; then
-		echo "FAIL";
+		echo $FAIL;
 		echo -e "\tUnable to determine renderer.";
 		continue;
 	fi
 
-	differences=$(
-		if [ 1 ]; then
-			echo "#! $renderer";
-			cat "$test" | "$REMD" -r "$renderer" - -;
-		fi | diff -w -y --suppress-common-lines - "$expected"
-	);
+	differences="$(
+		cat "$test" | "$REMD" -r "$renderer" - - \
+		| diff --ignore-matching-lines='^\s*#' --ignore-all-space \
+			--old-line-format="E%3dn|%L" --new-line-format="A%3dn|%L" --unchanged-line-format=" %3dn|%L" \
+			"$expected" - \
+		| grep -2 -E '^[AE]' \
+		| sed -r 's/^--/ ...|/' \
+		| sed -r 's/^E.*$/\x1b[0;32m&\x1b[0m/' | sed -r 's/^A.*$/\x1b[0;31m&\x1b[0m/'
+	)";
 
 	if [ "$differences" ]; then
-		echo "[FAIL]";
+		echo $FAIL;
 		echo "$differences";
 	else
-		echo "[PASS]";
+		echo $PASS;
 	fi
 done
