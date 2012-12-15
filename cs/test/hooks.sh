9b1179db690cbc590cb0701837c05a7925526a3d
James Cracknell <james.h.cracknell@gmail.com>
2012-12-15 14:33:11 -0700

    Test cases moved to repository root

---
+++ cs/test/hooks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SELF="$1"
+
+for build in Debug Release; do
+	REMD="$(dirname "$SELF")/../src/remd/bin/$build/remd.exe";
+	if [ -f "$REMD" ]; then break; fi
+done
+
+if [ ! -f "$REMD" ]; then
+	echo "cs: Unable to locate remd.exe - have you built the solution?";
+	exit 1;
+fi
+
+echo "# cs: REMD is \"$REMD\"";
+
+function RENDER() { renderer="$1"; test="$2";
+	"$REMD" "$test" -	
+}
